67fcdd0ad287bc5c798542af582e67ad93211a3c
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-10 14:04:48 -0700

    working on navbar

--- mockportfolio/src/App.js
+++ mockportfolio/src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import Home from "./pages/Home.js";
 import Projects from "./pages/Projects.js";
 import Experience from "./pages/Experience.js";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
