97cd0ebf1acea309b2f997aa6cbbc92d7f91ff87
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-17 13:22:50 -0700

    finished footer

--- mockportfolio/src/App.js
+++ mockportfolio/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home.js";
 import Projects from "./pages/Projects.js";
 import Experience from "./pages/Experience.js";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
--- mockportfolio/src/components/Footer.js
+++ mockportfolio/src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import "../styles/Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <a href="https://github.com/Muniba-Ahmed?tab=repositories">
+          <GitHubIcon />
+        </a>
+        <a href="https://twitter.com/MunibaAhmed786">
+          <TwitterIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/muniba-ahmed/">
+          <LinkedInIcon />
+        </a>
+        <a href="mailto:muniba1ahmed@gmail.com">
+          <EmailIcon />
+        </a>
+      </div>
+      <p> &copy; 2023 Muniba Ahmed</p>
+    </div>
+  );
 }
 
 export default Footer;
