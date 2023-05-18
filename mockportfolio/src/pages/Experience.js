34db7d90d7312cf0684feefb6fc261ed793de18c
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-17 17:46:25 -0700

    finished experience

--- mockportfolio/src/pages/Experience.js
+++ mockportfolio/src/pages/Experience.js
@@ -1,7 +1,52 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import SchoolIcon from "@material-ui/icons/School";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2014 - 2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            {" "}
+            University of California Berkeley, CA
+          </h3>
+          <p>Bachelors of Arts, Anthropology</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018 - 2020"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            {" "}
+            Norco College, CA
+          </h3>
+          <p>A.S in Chemistry</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2022 - 2023"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title"> BloomTech</h3>
+          <p>Full Time Program, Full Stack Web Development</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
