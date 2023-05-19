b603876c59f029303e31e468a152e8f38c57f397
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-19 14:55:04 -0700

    created helpers folder with ProjectList.js

--- mockportfolio/src/pages/Projects.js
+++ mockportfolio/src/pages/Projects.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
+import jobTracker from "../assets/jobTracker.png";
+import nasa from "../assets/nasa.png";
+import eComm from "../assets/eComm.png";
+import photographyWebsite from "../assets/photographyWebsite.png";
+import "../styles/Projects.css";
 
 function Projects() {
   return (
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
+        <ProjectItem name="Job Tracker" image={jobTracker} />
+        <ProjectItem name="Nasa Photo of the day" image={nasa} />
+        <ProjectItem
+          name="Photography Portfolio Website"
+          image={photographyWebsite}
+        />
+        <ProjectItem name="eCommerce Website" image={eComm} />
       </div>
     </div>
   );
