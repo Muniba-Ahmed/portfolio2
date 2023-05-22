d34d5066196a449105826f56b637de12e99ab6fe
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-22 13:26:14 -0700

    refactoring the projectItems in Projects.js

--- mockportfolio/src/pages/Projects.js
+++ mockportfolio/src/pages/Projects.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import jobTracker from "../assets/jobTracker.png";
-import nasa from "../assets/nasa.png";
-import eComm from "../assets/eComm.png";
-import photographyWebsite from "../assets/photographyWebsite.png";
+import { projectList } from "../helpers/ProjectList";
+
 import "../styles/Projects.css";
 
 function Projects() {
@@ -11,13 +9,9 @@ function Projects() {
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        <ProjectItem name="Job Tracker" image={jobTracker} />
-        <ProjectItem name="Nasa Photo of the day" image={nasa} />
-        <ProjectItem
-          name="Photography Portfolio Website"
-          image={photographyWebsite}
-        />
-        <ProjectItem name="eCommerce Website" image={eComm} />
+        {projectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
       </div>
     </div>
   );
