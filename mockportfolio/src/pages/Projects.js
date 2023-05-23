f25bedeb10f4fc5e4b229ae62568156a69893e25
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-22 21:52:01 -0700

    finished modal styling, no overlay

--- mockportfolio/src/pages/Projects.js
+++ mockportfolio/src/pages/Projects.js
@@ -1,24 +1,3 @@
-// import React from "react";
-// import ProjectItem from "../components/ProjectItem";
-// import { projectList } from "../helpers/ProjectList";
-
-// import "../styles/Projects.css";
-
-// function Projects() {
-//   return (
-//     <div className="projects">
-//       <h1>My Personal Projects</h1>
-//       <div className="projectList">
-//         {projectList.map((project) => {
-//           return <ProjectItem name={project.name} image={project.image} />;
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Projects;
-//*********************************************************** */
 import React, { useState } from "react";
 import ProjectItem from "../components/ProjectItem";
 import { projectList } from "../helpers/ProjectList";
@@ -62,7 +41,7 @@ function Projects() {
                 </div>
               </div>
               <div className="modal-content">
-                <p> {selectedProject.Skills}</p>
+                Skills: {selectedProject.Skills}
               </div>
             </div>
           )
@@ -78,48 +57,3 @@ function Projects() {
 }
 
 export default Projects;
-/************************************************************ */
-// import React, { useState } from "react";
-// import ProjectItem from "../components/ProjectItem";
-// import { projectList } from "../helpers/ProjectList";
-
-// import "../styles/Projects.css";
-
-// function Projects() {
-//   const [selectedProject, setSelectedProject] = useState(null);
-
-//   const handleProjectClick = (project) => {
-//     setSelectedProject(project);
-//   };
-
-//   return (
-//     <div className="projects">
-//       <h1>My Personal Projects</h1>
-//       <div className="projectList">
-//         {projectList.map((project) => {
-//           return (
-//             <ProjectItem
-//               key={project.id}
-//               name={project.name}
-//               image={project.image}
-//               onClick={() => handleProjectClick(project)}
-//             />
-//           );
-//         })}
-
-//         {selectedProject && (
-//           <div className="modal">
-//             <div className="modal-content">
-//               <h2>{selectedProject.name}</h2>
-//               <img src={selectedProject.image} alt={selectedProject.name} />
-//               <p> {selectedProject.Skills}</p>
-//               <button onClick={() => setSelectedProject(null)}>Close</button>
-//             </div>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Projects;
